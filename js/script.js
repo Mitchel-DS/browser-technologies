fd2926e72744237b6f7561cbb8299814f36937da
Mitchel-DS <mitchel.staal@outlook.com>
2023-04-06 03:01:51 +0200

    hide sections

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+console.log('script loaded');
+
 const form = document.querySelector('form');
+const sections = document.querySelectorAll('section');
+const infoButton = document.querySelector('#info-button');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -9,8 +13,19 @@ form.addEventListener('submit', (e) => {
 	const formDataString = JSON.stringify(formData);
 
 	localStorage.setItem('formData', formDataString);
-	alert('Form data saved to local storage');
-	console.log(formDataString);
+	console.log(formDataString + ' saved to local storage');
+
+	var student_name = document.getElementById("student-name").value;
+	var message = "<p>Results: " + student_name + "</p>";
+	document.getElementById("results-container").innerHTML = message;
 });
 
+for (let i = 1; i < sections.length; i++) {
+	sections[i].classList.add("js-active");
+  }
+
+infoButton.addEventListener('click', (e) => {
+	e.preventDefault();
+	sections[4].classList.remove("js-active");
+});
 
